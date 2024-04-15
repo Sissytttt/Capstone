c2f3c069547353d122af540d67454444f452fa9e
Sissytttt <st4228@nyu.edu>
2024-04-15 23:19:30 +0800

    add sound

---
+++ VisualizationMERGE/js/utils/getAudio.js
@@ -0,0 +1,20 @@
+function get_SingingBowl(path) {
+    const audioListener = new THREE.AudioListener();
+    camera.add(audioListener);
+    singingBowl = new THREE.Audio(audioListener);
+    scene.add(voiceOver);
+    const sloader = new THREE.AudioLoader();
+    sloader.load(
+        path,
+        function (audioBuffer) {
+            voiceOver.setBuffer(audioBuffer);
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (err) {
+            console.log('An error happened');
+        }
+    );
+    // voiceOver.play();
+}
